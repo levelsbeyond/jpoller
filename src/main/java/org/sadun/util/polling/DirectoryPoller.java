c53d6fa7779620f096199c01386d31ce5862091c
James Humphrey <james@levelsbeyond.com>
2014-09-19 15:24:26 -0600

    copyright

--- src/main/java/org/sadun/util/polling/DirectoryPoller.java
+++ src/main/java/org/sadun/util/polling/DirectoryPoller.java
@@ -1,7 +1,19 @@
-// Decompiled by Jad v1.5.8g. Copyright 2001 Pavel Kouznetsov.
-// Jad home page: http://www.kpdus.com/jad.html
-// Decompiler options: packimports(3)
-// Source File Name:   DirectoryPoller.java
+/*
+ * Levels Beyond CONFIDENTIAL
+ *
+ * Copyright 2003 - 2014 Levels Beyond Incorporated
+ * All Rights Reserved.
+ *
+ * NOTICE:  All information contained herein is, and remains
+ * the property of Levels Beyond Incorporated and its suppliers,
+ * if any.  The intellectual and technical concepts contained
+ * herein are proprietary to Levels Beyond Incorporated
+ * and its suppliers and may be covered by U.S. and Foreign Patents,
+ * patents in process, and are protected by trade secret or copyright law.
+ * Dissemination of this information or reproduction of this material
+ * is unlawful and strictly forbidden unless prior written permission is obtained
+ * from Levels Beyond Incorporated.
+ */
 
 package org.sadun.util.polling;
 
